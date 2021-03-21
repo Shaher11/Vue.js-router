4c28167802de8d9b21552e8faaea5b45732ea9be
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-03-21 05:46:18 +0200

    loading differant users and using watch to listen the ID change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" active-class="active" exact> <a>Home</a> </router-link>
-        <router-link to="/user" tag="li" active-class="active"> <a>User</a> </router-link>
+        <router-link to="/user/10" tag="li" active-class="active"> <a>User 10</a> </router-link>
+        <router-link to="/user/11" tag="li" active-class="active"> <a>User 11</a> </router-link>
+
     </ul>
 </template>
--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{ id }}</p>
         <button class="btn btn-primary" @click="navigateToHome">Back to Home</button>
     </div>
 
@@ -9,6 +10,16 @@
 
 <script>
     export default {
+        data(){
+            return{
+                id: this.$route.params.id
+            }
+        },
+        watch: {
+            '$route'(to, from){
+                this.id = to.params.id;
+            }
+        },
         methods:{
             navigateToHome(){
                 this.$router.push('/');
